ef7d95de314fe2854d7aaea4a5ec4cc6a3cfb6af
Shreyash Jagtap <shreyash5522@gmail.com>
2024-05-06 23:56:46 +0530

    Fix: Password Length Increased

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(250), nullable=False)
 
 @app.route('/signup', methods=['POST'])
 def signup():
